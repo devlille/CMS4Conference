d02d07aca538afebfc701a88fed4156acae7f998
Emmanuel <demey.emmanuel@gmail.com>
2024-02-17 21:01:39 +0100

    feat: create dedicated folder for cloud nourd

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as cms4devfestFunctions from "./cms4devfest";
-//import * as cms4devfestFunctionsGdg from "./cms4devfest-gdg";
+import * as cms4devfestFunctionsGdg from "./cms4devfest-gdg";
 
 export const cms = cms4devfestFunctions;
-//export const cms4devfestgdg = cms4devfestFunctionsGdg;
+export const cms4devfestgdg = cms4devfestFunctionsGdg;
 
 /*export const cms4devfestcloudnord = {
 };*/

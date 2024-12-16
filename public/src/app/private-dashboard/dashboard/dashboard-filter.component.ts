ab45726d311b5a2b2fb9d79c4d1e74754a370a55
Emmanuel <demey.emmanuel@gmail.com>
2024-12-16 11:17:34 +0000

    feat: review dashboard

---
+++ public/src/app/private-dashboard/dashboard/dashboard-filter.component.ts
@@ -0,0 +1,134 @@
+import { Component, computed, input, output } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+type FilterValueType = 'sign' | 'generated' | 'validated' | 'paid' | 'received' | 'communicated' | 'code';
+
+type Option = { value: FilterValueType | string; label: string };
+type Options = Option[];
+type OptionWithChecked = Option & { checked: boolean };
+
+export type Search = {
+  status: string[];
+  packs: string[];
+  types: string[];
+};
+
+const PackOptions: Options = [
+  { value: 'bronze', label: `Bronze` },
+  { value: 'silver', label: `Silver` },
+  { value: 'gold', label: `Gold` },
+  { value: 'party', label: `Party` }
+];
+const TypeOptions: Options = [
+  { value: 'esn', label: `ESN` },
+  { value: 'other', label: `Autres` },
+  { value: 'undefined', label: `Non défini` }
+];
+const StatusOptions: Options = [
+  { value: 'validated', label: `Valider` },
+  {
+    value: 'generated',
+    label: `Informations complémentaires`
+  },
+  { value: 'sign', label: `Signature` },
+  { value: 'paid', label: `Paiement` },
+  {
+    value: 'received',
+    label: `Eléments de communication`
+  },
+  {
+    value: 'communicated',
+    label: `Communication`
+  },
+  {
+    value: 'code',
+    label: `Code`
+  }
+];
+
+@Component({
+  selector: 'cms-dashboard-filter',
+  imports: [MatCheckboxModule, MatExpansionModule],
+  template: `
+    <div>
+      <mat-expansion-panel [expanded]="true">
+        <mat-expansion-panel-header>
+          <mat-panel-title> Par Statut </mat-panel-title>
+        </mat-expansion-panel-header>
+
+        @for (option of statusOptionsWithChecked(); track option) {
+          <mat-checkbox [checked]="option.checked" (change)="onChange('status', option.value)">{{ option.label }}</mat-checkbox>
+        }
+      </mat-expansion-panel>
+
+      <mat-expansion-panel [expanded]="true">
+        <mat-expansion-panel-header>
+          <mat-panel-title> Par Pack </mat-panel-title>
+        </mat-expansion-panel-header>
+        @for (option of packOptionsWithChecked(); track option) {
+          <mat-checkbox [checked]="option.checked" (change)="onChange('packs', option.value)">{{ option.label }}</mat-checkbox>
+        }
+      </mat-expansion-panel>
+
+      <mat-expansion-panel [expanded]="true">
+        <mat-expansion-panel-header>
+          <mat-panel-title> Par Type </mat-panel-title>
+        </mat-expansion-panel-header>
+        @for (option of typeOptionsWithChecked(); track option) {
+          <mat-checkbox [checked]="option.checked" (change)="onChange('types', option.value)">{{ option.label }}</mat-checkbox>
+        }
+      </mat-expansion-panel>
+    </div>
+  `
+})
+export class DashboardFilterComponent {
+  model = input<Search>();
+  modelChange = output<Search>();
+
+  private convertToOptionWithChecked = (options: Options, key: keyof Search) => {
+    return options.map((option) => {
+      return {
+        ...option,
+        checked: this.model()?.[key].includes(option.value) ?? false
+      };
+    });
+  };
+  statusOptionsWithChecked = computed<OptionWithChecked[]>(() => this.convertToOptionWithChecked(StatusOptions, 'status'));
+  packOptionsWithChecked = computed<OptionWithChecked[]>(() => this.convertToOptionWithChecked(PackOptions, 'packs'));
+  typeOptionsWithChecked = computed<OptionWithChecked[]>(() => this.convertToOptionWithChecked(TypeOptions, 'types'));
+
+  onChange(onChangeKey: keyof Search, onChangeValue: any) {
+    const currentValue = this.model();
+    if (!currentValue) {
+      return;
+    }
+
+    const newValue = Object.entries(currentValue).reduce(
+      (acc: Search, [key, values]: [string, string[]]) => {
+        const computeValue = () => {
+          if (key === onChangeKey) {
+            const isPresent = values.find((a) => a === onChangeValue);
+            if (!isPresent) {
+              return [...values, onChangeValue];
+            } else {
+              return [...values.filter((e) => e !== onChangeValue)];
+            }
+          }
+          return values;
+        };
+        return {
+          ...acc,
+          [key]: computeValue()
+        };
+      },
+      {
+        status: [],
+        packs: [],
+        types: []
+      }
+    );
+
+    this.modelChange.emit(newValue);
+  }
+}
--- public/src/app/private-dashboard/dashboard/dashboard.component.ts
+++ public/src/app/private-dashboard/dashboard/dashboard.component.ts
@@ -12,62 +12,61 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
-import { Company, Configuration, PartnerType } from '../../model/company';
+import { Company, Configuration, WorkflowStatus } from '../../model/company';
 import { PartnerService } from '../../services/partner.service';
-
-type FilterValueType = 'sign' | 'generated' | 'validated' | 'paid' | 'received' | 'communicated' | 'code';
-type FilterByPackValueType = string;
-type FilterByType = PartnerType | 'undefined';
+import { DashboardFilterComponent, Search } from './dashboard-filter.component';
 
 @Component({
   selector: 'cms-dashboard',
-  imports: [MatCheckboxModule, MatExpansionModule, CommonModule, MatRadioModule, MatTableModule, FormsModule, MatCardModule, MatButtonModule, MatSortModule, MatButtonToggleModule],
+  imports: [
+    DashboardFilterComponent,
+    MatCheckboxModule,
+    MatExpansionModule,
+    CommonModule,
+    MatRadioModule,
+    MatTableModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSortModule,
+    MatButtonToggleModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements AfterViewInit {
+  value = signal<Search>({
+    status: ['sign', 'generated', 'validated', 'paid', 'received', 'communicated', 'code'],
+    packs: ['bronze', 'silver', 'gold', 'party'],
+    types: ['esn', 'other', 'undefined']
+  });
+
   readonly sort = viewChild.required(MatSort);
+
+  onFilterChange = (search: Search) => {
+    this.value.set(search);
+  };
 
   configuration = signal<Configuration | undefined>(undefined);
   displayedColumns: string[] = ['creationDate', 'name', 'sponsoring', 'secondSponsoring', 'action'];
   partners: Partial<Company>[] = [];
-  filterByStatus: { value: FilterValueType; label: string }[] = [];
 
   originalPartners = signal<Partial<Company>[]>([]);
-  filterByStatusValue = signal<FilterValueType[]>([]);
 
   shouldDisplayRelanceButton = linkedSignal(() => {
-    const status = this.filterByStatusValue();
+    const status = this.value().status;
     return status.length === 1 && ['generated', 'sign', 'paid'].indexOf(status[0]) >= 0;
-  });
-  filterByTypeValue = signal<FilterByType[]>(['esn', 'other', 'undefined']);
-
-  filteredPartnersByStatus = linkedSignal(() => {
-    const status = this.filterByStatusValue();
-    console.log(
-      { status, partners: this.originalPartners() },
-      this.originalPartners().filter((partner) => status.find((v) => !!partner.status && partner.status[v] === 'pending'))
-    );
-
-    return this.originalPartners().filter((partner) => status.find((v) => !!partner.status && partner.status[v] === 'pending'));
-  });
-
-  filteredPartnersByStatusAndPacks = linkedSignal(() => {
-    const packs = this.filterByPackValue();
-
-    return this.filteredPartnersByStatus().filter((partner) => {
-      if (!partner.sponsoring) {
-        return false;
-      }
-      return packs.indexOf(partner.sponsoring.toLowerCase() as FilterByPackValueType) >= 0;
-    });
   });
 
   filteredPartners = computed(() => {
-    const types = this.filterByTypeValue();
-    return this.filteredPartnersByStatusAndPacks().filter((partner) => {
-      return types.indexOf(partner.type!) >= 0 || (!partner.type && types.indexOf('undefined') >= 0);
+    const partners = this.originalPartners();
+
+    const filteredByStatus = partners.filter((p) => this.value().status.find((s: any) => p.status?.[s as unknown as keyof WorkflowStatus] === 'pending'));
+    const filteredByPack = filteredByStatus.filter((p) => this.value().packs.includes(p.sponsoring!));
+    const filterByType = filteredByPack.filter((p) => {
+      return this.value().types.includes(p.type!) || (this.value().types.includes('undefined') && !p.type);
     });
+    return filterByType;
   });
 
   dataSource = linkedSignal(() => {
@@ -76,78 +75,11 @@ export class DashboardComponent implements AfterViewInit {
     return dataSource;
   });
 
-  packOptions = computed(() => {
-    const partners = this.filteredPartnersByStatus();
-    const configuration = this.configuration();
-
-    const stats = partners.reduce(
-      (acc: Record<string, number>, partner: Partial<Company>) => {
-        const count: number = acc[partner.sponsoring!.toLocaleLowerCase()] ?? 0;
-        return {
-          ...acc,
-          [partner.sponsoring!.toLocaleLowerCase()]: count + 1
-        };
-      },
-      {
-        ...configuration?.sponsorships.reduce((acc, sponsorship) => ({ ...acc, [sponsorship.name]: 0 }), {})
-      }
-    );
-
-    return Object.entries(stats).map(([key, count]: [string, number]) => ({
-      value: key,
-      label: `${key} (${count})`
-    }));
-  });
-
-  filterByPackValue = computed(() => {
-    const options = this.packOptions();
-    const filterByPackSelected = this.filterByPackSelected();
-    const values = options.map((option) => option.value);
-
-    if (!filterByPackSelected) {
-      return values;
-    }
-    return values.filter((value) => filterByPackSelected.includes(value));
-  });
-  filterByPackSelected = signal<FilterByPackValueType[]>([]);
-
-  filterByPackHandler(value: FilterByPackValueType) {
-    const isPresent = !!this.filterByPackSelected().find((a) => a === value);
-    if (!isPresent) {
-      this.filterByPackSelected.set([...this.filterByPackSelected(), value]);
-    } else {
-      this.filterByPackSelected.set([...this.filterByPackSelected().filter((e) => e !== value)]);
-    }
-  }
-
-  filterByType = linkedSignal(() => {
-    const partners = this.filteredPartnersByStatusAndPacks();
-
-    let numberESN = 0;
-    let numberOther = 0;
-    let numberUndefined = 0;
-    partners.forEach((partner) => {
-      if (partner.type === 'esn') {
-        numberESN++;
-      } else if (partner.type === 'other') {
-        numberOther++;
-      } else {
-        numberUndefined++;
-      }
-    });
-
-    return [
-      { value: 'esn', label: `ESN (${numberESN})` },
-      { value: 'other', label: `Autres (${numberOther})` },
-      { value: 'undefined', label: `Non défini (${numberUndefined})` }
-    ];
-  });
-
   private readonly partnerService: PartnerService = inject(PartnerService);
   private readonly functions: Functions = inject(Functions);
 
   async relance() {
-    const status = this.filterByStatusValue();
+    const status = this.value().status;
     if (status[0] === 'generated') {
       await httpsCallable(this.functions, 'cms-relanceInformationPourGeneration')();
     } else if (status[0] === 'sign') {
@@ -163,12 +95,10 @@ export class DashboardComponent implements AfterViewInit {
     this.partnerService.update(id, { archived: true });
   }
 
-  filterByValue(filterValue: FilterValueType[]) {
-    this.filterByStatusValue.set(filterValue);
-  }
-
   ngAfterViewInit() {
-    this.partnerService.getCurrentConfiguration().then((config) => this.configuration.set(config));
+    this.partnerService.getCurrentConfiguration().then((config) => {
+      this.configuration.set(config);
+    });
 
     this.partnerService.getAll().subscribe((partners) => {
       this.originalPartners.set(
@@ -183,67 +113,7 @@ export class DashboardComponent implements AfterViewInit {
           needAction: (!!partners.conventionSignedUrl && partners.status!.sign === 'pending') || (partners.status!.generated === 'pending' && !!partners.address)
         }))
       );
-      this.countByStep(partners);
     });
-  }
-  countByStep(partners: Company[]) {
-    const counter: Record<string, number> = {
-      sign: 0,
-      generated: 0,
-      validated: 0,
-      paid: 0,
-      received: 0,
-      communicated: 0,
-      code: 0
-    };
-
-    partners.forEach((partner) => {
-      Object.entries(partner.status ?? {}).forEach(([step, status]) => {
-        if (status === 'pending') {
-          counter[step] += 1;
-        }
-      });
-    });
-
-    this.filterByStatus = [
-      { value: 'validated', label: `Valider (${counter['validated']})` },
-      {
-        value: 'generated',
-        label: `Informations complémentaires (${counter['generated']})`
-      },
-      { value: 'sign', label: `Signature (${counter['sign']})` },
-      { value: 'paid', label: `Paiement (${counter['paid']})` },
-      {
-        value: 'received',
-        label: `Eléments de communication (${counter['received']})`
-      },
-      {
-        value: 'communicated',
-        label: `Communication (${counter['communicated']})`
-      },
-      {
-        value: 'code',
-        label: `Code (${counter['code']})`
-      }
-    ];
-  }
-
-  filterByStatusValueHandler(values: FilterValueType) {
-    const isPresent = !!this.filterByStatusValue().find((a) => a === values);
-    if (!isPresent) {
-      this.filterByStatusValue.set([...this.filterByStatusValue(), values]);
-    } else {
-      this.filterByStatusValue.set([...this.filterByStatusValue().filter((e) => e !== values)]);
-    }
-  }
-
-  filterByTypeHandler(value: any) {
-    const isPresent = !!this.filterByTypeValue().find((a) => a === value);
-    if (!isPresent) {
-      this.filterByTypeValue.set([...this.filterByTypeValue(), value]);
-    } else {
-      this.filterByTypeValue.set([...this.filterByTypeValue().filter((e) => e !== value)]);
-    }
   }
 
   copyEmails() {
